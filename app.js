22a79118b22401a8377a44c054b1a82c9bf244fa
Mujthahid <mujthahidk@gmail.com>
2022-08-11 22:40:02 +0530

    admin page updated and add product function added

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// installed handlebars
 var hbs = require('express-handlebars')
+// assigned user and admin routes
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
-
+// installed fileupload
+var fileUpload =require('express-fileupload')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+// assigned directories and pathname
 app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload())
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
